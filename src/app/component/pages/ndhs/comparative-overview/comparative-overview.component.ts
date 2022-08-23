ba5eb38a38b9ee8f2ff799565112e7731554c4be
Neeraj <106156393+neerajmangoit@users.noreply.github.com>
2022-08-23 09:42:19 +0530

    Charts

--- src/app/component/pages/ndhs/comparative-overview/comparative-overview.component.ts
+++ src/app/component/pages/ndhs/comparative-overview/comparative-overview.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { EChartsOption } from 'echarts/types/dist/echarts';
 
 @Component({
     selector: 'app-comparative-overview',
@@ -6,6 +7,57 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./comparative-overview.component.css'],
 })
 export class ComparativeOverviewComponent implements OnInit {
-    ngOnInit() {
+
+
+    chartOptionRadar: EChartsOption = {
+        title: {
+
+        },
+        legend: {
+          data: ['Allocated Budget', 'Actual Spending']
+        },
+        radar: {
+          shape: 'circle',
+                splitArea: {
+              areaStyle: {
+                color: ['#FFFAE3', '#F0EFEF'],
+                shadowColor: 'rgba(0, 0, 0, 0)',
+                shadowBlur: 10
+              }
+            },
+          indicator: [
+            { name: 'Sales', max: 6500 },
+            { name: 'Administration', max: 16000 },
+            { name: 'Information Technology', max: 30000 },
+            { name: 'Customer Support', max: 38000 },
+          ]
+        },
+        series: [
+          {
+            name: 'Budget vs spending',
+            type: 'radar',
+            data: [
+              {
+                value: [4200, 9000, 20000, 32000, 50000, 180000],
+                name: 'Allocated Budget',
+
+              },
+              {
+                value: [5000, 14000, 28000, 26000, 42000, 21000],
+                name: 'Actual Spending',
+                areaStyle: {
+
+
+                }
+              }
+            ]
+          }
+        ]
+      };
+
+
+    ngOnInit(): void {
+
     }
+
 }
--- src/app/component/pages/ndhs/comparative-result-detail/comparative-result-detail.component.ts
+++ src/app/component/pages/ndhs/comparative-result-detail/comparative-result-detail.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as am5 from '@amcharts/amcharts5';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import * as am5map from '@amcharts/amcharts5/map';
+import am5geodata_worldLow from '@amcharts/amcharts5-geodata/worldLow';
 
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import { EChartsOption } from 'echarts/types/dist/echarts';
 
 @Component({
     selector: 'app-comparative-result-detail',
     templateUrl: './comparative-result-detail.component.html',
     styleUrls: ['./comparative-result-detail.component.css'],
 })
-export class ComparativeResultDetailComponent {
-   
+export class ComparativeResultDetailComponent implements OnInit {
+    ngOnInit(): void {
+        // Create root and chart
+        // Create root and chart
+    }
+
+    chartOptionBar: EChartsOption = {
+        title: {
+            text: 'Health Governance',
+        },
+        legend: {},
+        tooltip: {},
+        dataset: {
+            source: [
+                ['Readiness'],
+                ['Japan', 60],
+                ['Australia', 80],
+                ['India', 50],
+                ['China', 60],
+                ['Spain', 60],
+            ],
+        },
+        xAxis: {
+            type: 'category',
+            axisLabel: { interval: 0, rotate: 30 },
+        },
+        yAxis: {},
+        // Declare several bar series, each will be mapped
+        // to a column of dataset.source by default.
+        series: [{ type: 'bar' }],
+    };
 }
