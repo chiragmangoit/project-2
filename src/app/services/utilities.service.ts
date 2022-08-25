7764a1eeb4d5d32b942df3ef808512514129f73c
chiragmangoit <106661890+chiragmangoit@users.noreply.github.com>
2022-08-25 15:03:23 +0530

    Revert "Neeraj"

--- src/app/services/utilities.service.ts
+++ src/app/services/utilities.service.ts
@@ -5,15 +5,6 @@ import { BehaviorSubject, Subject } from 'rxjs';
     providedIn: 'root',
 })
 export class UtilitiesService {
-    defaultNodeData = {
-        d_id: "1,2",
-        g_id: localStorage.getItem('governance_id'),
-        t_id: 1,
-        u_id: 2,
-        countries: localStorage.getItem('selected_country'),
-        year: "2022,2021",
-    };
-
     defaultCountry = {
         countries: localStorage.getItem('selected_country'),
     };
@@ -21,6 +12,5 @@ export class UtilitiesService {
     emitGovernance = new BehaviorSubject<any>(1);
     header = new BehaviorSubject<boolean>(false);
     emitDefaultCountries = new BehaviorSubject<any>(this.defaultCountry);
-    emitDataModel = new Subject<any>();
-    emitNodeData = new BehaviorSubject<any>(this.defaultNodeData);
+    emitDataModel = new Subject<any>()
 }
